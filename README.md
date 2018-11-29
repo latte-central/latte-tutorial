8e09bd3d01a4f66617f8397870c1272afc369169
fredokun <frederic.peschanski@lip6.fr>
2018-11-29 15:35:38 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+# latte-tutorial
+
+This is a tutorial about the LaTTe proof assistant,
+ currently being written...
+
+## License
+
+Copyright © 2018 Frederic Peschanski, the MIT License (cf. `LICENSE`)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject latte-tutorial "0.1.0-SNAPSHOT"
+  :description "A gentle introduction to the LaTTe proof assistant."
+  :url "https://github.com/latte-central/latte-tutorial"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [latte "1.0b1-SNAPSHOT"]])
