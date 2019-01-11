f0bd59a88805ee634d514a14a644ad1fb6fee534
Frederic Peschanski <not provided>
2019-01-11 22:11:05 +0100

    beta3 deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject latte-tutorial "0.1.0-SNAPSHOT"
+(defproject latte-tutorial "0.2.0-SNAPSHOT"
   :description "A gentle introduction to the LaTTe proof assistant."
   :url "https://github.com/latte-central/latte-tutorial"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [latte "1.0b2-SNAPSHOT"]
-                 [latte-prelude "1.0b2-SNAPSHOT"]
-                 [latte-sets "1.0b2-SNAPSHOT"]
-                 [latte-integers "1.0b2-SNAPSHOT"]]
-  :plugins [[cider/cider-nrepl "0.20.0-SNAPSHOT"]])
+                 [latte "1.0b3-SNAPSHOT"]
+                 [latte-prelude "1.0b3-SNAPSHOT"]
+                 [latte-sets "1.0b3-SNAPSHOT"]
+                 [latte-integers "1.0b3-SNAPSHOT"]])
+
 
